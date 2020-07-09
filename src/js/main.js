ab90af4ec31753b57ac516c74c379a82731d59f1
AntonVoronezh <pstut@mail.ru>
2020-07-09 16:55:29 +0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+import "./lib/lib";
+
+// $('.active').hide();
+// $("button").on("click", function () {
+//   $("div").eq(2).toggleClass("active");
+// });
+
+// $("div").click(function () {
+//   console.log($(this).index());
+// });
+
+console.log($('div').eq(2).find2('.some'))
+// console.log($('div').find2('.some'))
