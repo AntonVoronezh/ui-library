ada29768bb9807bfaf8bd445f3a184b72759e1e6
AntonVoronezh <pstut@mail.ru>
2020-07-09 16:55:10 +0300

    init

---
+++ src/js/lib/modules/handlers.js
@@ -0,0 +1,34 @@
+import $ from "../core";
+
+$.prototype.on = function (eventMane, callback) {
+  if (!eventMane || !callback) {
+    return this;
+  }
+
+  for (let i = 0; i < this.lenght; i++) {
+    this[i].addEventListener(eventMane, callback);
+  }
+  return this;
+};
+
+$.prototype.off = function (eventMane, callback) {
+  if (!eventMane || !callback) {
+    return this;
+  }
+
+  for (let i = 0; i < this.lenght; i++) {
+    this[i].removeEventListener(eventMane, callback);
+  }
+  return this;
+};
+
+$.prototype.click = function (callback) {
+  for (let i = 0; i < this.lenght; i++) {
+    if (callback) {
+      this[i].addEventListener("click", callback);
+    } else {
+      this[i].click();
+    }
+  }
+  return this;
+};
