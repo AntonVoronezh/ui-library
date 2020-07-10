da8edf619ca75ede27c9e5316476ba11fd1de4bb
AntonVoronezh <pstut@mail.ru>
2020-07-10 10:17:40 +0300

    effects

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
 import "./lib/lib";
 
-// $('.active').hide();
+// console.log($('div').show());
 // $("button").on("click", function () {
-//   $("div").eq(2).toggleClass("active");
+// $("div").eq(2).toggleClass("active");
 // });
 
-// $("div").click(function () {
-//   console.log($(this).index());
-// });
+$("div").click(function () {
+  console.log($(this).index());
+});
 
-console.log($('div').eq(2).find2('.some'))
-// console.log($('div').find2('.some'))
+// console.log($('div').eq(2).find2('.some'))
+// console.log($('.some').closest('.findme'))
+
+// console.log($(".more").eq(0).siblings());
+// console.log($(".findme").siblings());
+console.log($(".findme").fadeIn(2000, ()=>console.log('111')));
