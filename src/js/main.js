11f62aef059b795c3c367c4d86a0c7b8ce3f9af6
AntonVoronezh <pstut@mail.ru>
2020-07-16 11:46:34 +0300

    effects

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,31 @@
 import "./lib/lib";
 
-// console.log($('div').show());
-// $("button").on("click", function () {
-// $("div").eq(2).toggleClass("active");
+// // console.log($('div').show());
+// // $("button").on("click", function () {
+// // $("div").eq(2).toggleClass("active");
+// // });
+//
+// $("div").click(function () {
+//   console.log($(this).index());
 // });
+//
+// // console.log($('div').eq(2).find2('.some'))
+// // console.log($('.some').closest('.findme'))
+//
+// // console.log($(".more").eq(0).siblings());
+// // console.log($(".findme").siblings());
+// console.log($(".findme").fadeIn(2000, ()=>console.log('111')));
 
-$("div").click(function () {
-  console.log($(this).index());
+$("#first").on("click", () => {
+  $("div").eq(1).fadeOut(1000);
 });
 
-// console.log($('div').eq(2).find2('.some'))
-// console.log($('.some').closest('.findme'))
+$('[data-ffff="second"]').on("click", () => {
+  $("div").eq(2).fadeOut(1000);
+});
 
-// console.log($(".more").eq(0).siblings());
-// console.log($(".findme").siblings());
-console.log($(".findme").fadeIn(2000, ()=>console.log('111')));
+$("button")
+  .eq(2)
+  .on("click", () => {
+    $("div").fadeOut(1000);
+  });
