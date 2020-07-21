8dff1023f94e544ddc8bd9aae15c15dce38de5f6
AntonVoronezh <pstut@mail.ru>
2020-07-21 10:33:30 +0300

    effects

--- src/js/lib/components/dropdown.js
+++ src/js/lib/components/dropdown.js
@@ -4,11 +4,10 @@ $.prototype.dropdown = function () {
   for (let i = 0; i < this.lenght; i++) {
     const id = this[i].getAttribute("id");
 
-    $(this[i]).on("click", () => {
-      $(`[data-toggle-id="${id}]"`).fadeToggle(300);
+    $(this[i]).click( () => {
+       $(`[data-toggle-id="${id}"]`).fadeToggle(300);
     });
   }
 };
 
-// console.log($(".dropdown-toggle"))
 $(".dropdown-toggle").dropdown();
